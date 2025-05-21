3ae1de42f62da56b1c4f90aee96c8f1867099b1a
faroutnerd <farhan91179@gmail.com>
2025-05-21 19:16:29 +0530

    few changes

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 import { createContext, useEffect, useRef, useState } from "react";
-import {assets} from '../assets/assets';
 
 export const PlayerContext = createContext();
 
